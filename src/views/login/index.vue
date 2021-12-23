4b25c109af808245c02c24c2c59ed76250335c76
tangyue <chaoslover@126.com>
2021-12-23 22:10:22 +0800

    # 登录去掉默认账号、密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -68,8 +68,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'iGrXus2itsdkowcjhfj4'
+        username: '', // admin
+        password: '' // iGrXus2itsdkowcjhfj4
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
